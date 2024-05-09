aa118010b77530419b4f5079f77a2b215aa09e21
Nikolay Tzolov <ntzolov@gmail.com>
2024-05-09 20:45:53 +0300

    add animation

--- tailwind-playground/tailwind.config.js
+++ tailwind-playground/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/index.css', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/index.css", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        moveInCircle: {
+          "0%": { transform: "translate(0, 0)" },
+          "25%": { transform: "translate(0, 150px)" },
+          "50%": { transform: "translate(150px, 150px)" },
+          "75%": { transform: "translate(150px, 0)" },
+          "100%": { transform: "translate(0, 0)" },
+        },
+      },
+      animation: {
+        moveInCircle: "moveInCircle 4s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
